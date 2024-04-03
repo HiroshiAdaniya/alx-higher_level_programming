e3d1981856d34b45de9fa7d762bdc8d9f502c341
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-04-03 17:05:04 +0200

    added: 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: a singly linked list
+ * Return: 0 if no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *hare, *tortoise;
+
+	hare = tortoise = list;
+
+	while (hare != NULL && hare->next != NULL)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (hare == NULL)
+			return (0);
+		else if (tortoise == hare)
+			break;
+	}
+	return (1);
+}
