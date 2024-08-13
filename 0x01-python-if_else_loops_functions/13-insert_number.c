ebad14247afa5559c06a16b66419705bc8970ee2
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-08-13 16:03:38 +0200

    debugging: 13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -11,9 +11,9 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *start = *head;
 	listint_t *tmp = start;
 
-	if (*head == NULL)
+	/*if (*head == NULL)
 		return (NULL);
-
+*/
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
