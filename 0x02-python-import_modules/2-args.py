fd55877d330bbfdf39c3f96a4c486695e37f4706
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-08-21 19:30:35 +0200

    debugging

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-i = len(sys.argv)
+if __name__ == "__main__":
+    import sys
+    i = len(sys.argv)
 
-if i == 1:
-    print("0 arguments.")
-elif i == 2:
-    print("1 argument:\n1: {}".format(sys.argv[1]))
-else:
-    print("{} arguments:".format(i - 1))
-    for j in range(1, i):
-        print("{}: {}".format(j, sys.argv[j]))
+    if i == 1:
+        print("0 arguments.")
+    elif i == 2:
+        print("1 argument:\n1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(i - 1))
+        for j in range(1, i):
+            print("{}: {}".format(j, sys.argv[j]))
