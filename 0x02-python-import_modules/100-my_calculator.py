bba514aaccca987966d79c3d4e01fffa03b7ec17
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-08-22 17:26:51 +0200

    added: 100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import *
+    length = len(sys.argv)
+    if (length != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    elif sys.argv[2] == '+' and '-' and '*' and '/':
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        operator = sys.argv[2]
+        if sys.argv[2] == '+':
+            print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+        elif sys.argv[2] == '-':
+            print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+        elif sys.argv[2] == '*':
+            print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+        else:
+            print("{} {} {} = {}".format(a, operator, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
