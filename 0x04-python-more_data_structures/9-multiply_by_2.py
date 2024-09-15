b851b28d75052d177cd7acb430783772a1316aad
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-09-15 23:28:42 +0200

    added: 9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_diction = {}
+
+    for i, j in a_dictionary.items():
+        new_diction.update(dict([(i, j * 2)]))
+    return new_diction
