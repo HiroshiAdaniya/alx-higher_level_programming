7e96d8aefebc20cbc7523bda2b9e297aee5054eb
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-09-18 19:11:14 +0200

    debugging: 101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new_matrix = matrix[:]
-    for i in range(len(matrix)):
-        new_matrix[i] = list(map(lambda x: x ** 2, matrix[i]))
-    return new_matrix
+    return (list(map(lambda x: list(map(lambda y: y ** 2, matrix[x])),
+            range(len(matrix)))))
